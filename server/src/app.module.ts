3273b72b6fc5f889e4480a94972bd033e47ff937
TrueRegin <36866793+TrueRegin@users.noreply.github.com>
2021-04-06 00:57:21 -0400

    Changed settings to work in production

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,14 +5,16 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         ServeStaticModule.forRoot({
             rootPath: './client',
             serveRoot: '/'
         }),
-        MongooseModule.forRoot('mongodb://localhost:27017/nuxt-feed'),
+        MongooseModule.forRoot(process.env.MONGO_CONNECTION),
         PostsModule,
     ],
     controllers: [AppController],
